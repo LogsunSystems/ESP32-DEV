9aa97bcc19ef1dd3752e9413925a8c30cc237141
LogsunSystems <108651919+LogsunSystems@users.noreply.github.com>
2022-07-06 17:35:20 +0530

    Updated the information header

--- Sample Codes/I2C_EEPROM/I2C_EEPROM.ino
+++ Sample Codes/I2C_EEPROM/I2C_EEPROM.ino
@@ -1,3 +1,21 @@
+/*
+* Company:       Logsun Systems
+* mail_ID:       info@logsun.com
+*
+* Platform:      ESP32-Dev Board
+* Title:         I2C EEPROM Sample Code
+* Description:   The code performs following operations to test the onboard I2C EEPROM
+                 - Writes ASCII characters from 33 to 127 in EPPTOM
+                 - Read the EEPROM locations to verify the Write Operation
+                 - Upload this code to board
+                 - Open the Serial Terminal, Set the baud rate to 115200.
+                 - Observe ethe output
+                   
+* Date:         06th October 2021
+* author:       Shreyas Deshpande.
+*/
+
+
 #include "I2C_EEPROM.h"
 
 int Address =0;
